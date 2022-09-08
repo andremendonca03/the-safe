34a94858ed86e8a998b2485596dd1c38c715fb08
Andre Mendonca <andremendonca.033@gmail.com>
2022-09-08 11:45:46 +1000

    Main Component

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Card from "./Card";
+
+const Main = () => {
+
+  return (
+    <>
+      <NavBar />
+      <Card />
+    </>
+  );
+};
+
+export default Main;
