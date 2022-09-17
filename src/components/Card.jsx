c12dfdd1f24334349b19b2cd55902b076714126d
Andre Mendonca <andremendonca.033@gmail.com>
2022-09-17 14:43:15 +1000

    Card structure Update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import styles from './Card.module.css';
-import Balance from './accounts/Balance';
-import Methods from './accounts/Methods';
-import Graphic from './accounts/Graphic';
-import AddBtns from './accounts/AddBtns';
-import SideInfo from './accounts/SideInfo';
+import React from "react";
+import styles from "./Card.module.css";
+
+import Transactions from "./transactions/Transactions";
+import Method from "./transactions/Method";
+
+import Balance from "./accounts/Balance";
+import Methods from "./accounts/Methods";
+import Graphic from "./accounts/Graphic";
+import AddBtns from "./accounts/AddBtns";
+import SideInfo from "./accounts/SideInfo";
+
+import { GlobalContext } from "../GlobalContext";
 
 const Card = () => {
+  const global = React.useContext(GlobalContext);
+
+  const isTransactions = global.page === "transactions";
+  const isAccounts = global.page === "account";
+  const isCategories = global.page === "categories";
+
   return (
     <main className={styles.main}>
-      <section className={styles.topComponents}>
-        <Balance />
-        <AddBtns />
-      </section>
-      <section className={styles.bottomComponents}>
-        <Methods />
-        <Graphic />
-        <SideInfo />
-      </section>
+      {isTransactions ? (
+        <article>
+          <section className={styles.topComponents}>
+            <Balance />
+            <Method />
+          </section>
+          <section className={styles.bottomComponents}>
+            <Transactions />
+          </section>
+        </article>
+      ) : (
+        <></>
+      )}
+
+      {isAccounts ? (
+        <article>
+          <section className={styles.topComponents}>
+            <Balance />
+            <AddBtns />
+          </section>
+          <section className={styles.bottomComponents}>
+            <Methods />
+            <Graphic />
+            <SideInfo />
+          </section>
+        </article>
+      ) : (
+        <></>
+      )}
+
+      {isCategories ? (
+        <article>
+
+        </article>
+      ) : (
+        <></>
+      )}
     </main>
-  )
-}
+  );
+};
 
 export default Card;
