c14a1d7bdedccbb5b79377191bbb363f44280412
Andre Mendonca <andremendonca.033@gmail.com>
2022-09-17 14:41:09 +1000

    Main Bg Update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import styles from "./Main.module.css";
 import NavBar from "./NavBar";
 import Card from "./Card";
 
 const Main = () => {
 
   return (
-    <>
+    <div className={styles.mainBg}>
       <NavBar />
       <Card />
-    </>
+    </div>
   );
 };
 
