4a8748b7f4d025071b9daad4e61f3c96bb53ecfd
Andre Mendonca <andremendonca.033@gmail.com>
2022-09-18 14:47:39 +1000

    TotalExpenses - categories Component

---
+++ src/components/categories/TotalExpenses.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./TotalExpenses.module.css";
+import { GlobalContext } from "../../GlobalContext";
+
+const TotalExpenses = () => {
+  const global = React.useContext(GlobalContext);
+
+  return (
+    <div className={styles.totalExpenses}>
+      Total Expenses:{" "}
+      <span>
+        $ {global.balanceHidden ? "— — —" : global.numbers.expenses("all")}
+      </span>
+    </div>
+  );
+};
+
+export default TotalExpenses;
